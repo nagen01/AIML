361855effda5121a7ebafebf85c8840933c329fa
nagen01 <50768930+nagen01@users.noreply.github.com>
2019-06-22 16:42:27 +0100

    Add files via upload

---
+++ Artificial Intelligence A-Z/Part 1 - Deep Q-Learning/Module 1 - Self Driving Car/selfcar.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 07:29:52 2019
+
+@author: Nagendra
+"""
+
+#Self Driving car
---
+++ Artificial Intelligence A-Z/Part 2 - Deep Convolutional Q-Learning/Module 2 - Doom/Artificial intelligence.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 21 07:21:45 2019
+
+@author: Nagendra
+"""
+#AI for doom
